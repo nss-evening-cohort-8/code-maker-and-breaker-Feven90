69bfc33916ded88a918cb0693379e7db5e6e006c
Feven Mulugeta <f.mulugeta90@gmail.com>
2018-09-22 15:33:10 -0500

    code breaker

--- main.js
+++ main.js
@@ -9,24 +9,21 @@ secretButton.addEventListener ('click', () => {
     const word = document.getElementById("user_input").value;
     let input = word.split("");    
     for (let i=0; i<input.length; i++){
-        input2 = input.join('');
-const secret = `${input2.charCodeAt(i)}`;
+const secret = word.charCodeAt(input[i]);
 printToDom(secret,"printSecret");
         };
 
 });
 
-
-
 const convertButton = document.getElementById("secret2");
 convertButton.addEventListener ('click', () => {
     const word = document.getElementById("user_input2").value;
+
     let input = word.split(",");    
     for (let i=0; i<input.length; i++){
-        input2 = input.join('');
-const secret = String.fromCharCode(word);
+const secret = String.fromCharCode(input[i]);
+console.log(secret);
 printToDom(secret,'convert');
         };
-      window.onload;
 });
 
